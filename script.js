bcf85f2bf8eec51ae0ce6922b12fd6785d81f692
Samib93 <sami.barkaoui93290@gmail.com>
2024-04-03 11:27:25 +0200

    correction bug carrousel

--- script.js
+++ script.js
@@ -2,27 +2,27 @@ document.addEventListener('DOMContentLoaded', function() {
     const carouselInner = document.querySelector('.carousel-inner');
     const images = carouselInner.children;
     const totalImages = images.length;
-    const imageWidth = images[0].offsetWidth; // Largeur d'une image en pixels
-    const visibleImages = 3; // Nombre d'images visibles à la fois
+    const imageWidth = images[0].offsetWidth; 
+    const visibleImages = 3; 
     let currentIndex = 0;
 
-    // Changer le temps d'intervalle ici (en millisecondes)
-    const intervalTime = 5000; // par exemple, 5000 millisecondes (5 secondes)
+    
+    const intervalTime = 5000; 
 
     setInterval(function() {
         currentIndex++;
 
-        // Déplacer le carrousel vers la prochaine image
-        carouselInner.style.transition = 'transform 1s ease'; // Activer la transition pour un défilement fluide
+       
+        carouselInner.style.transition = 'transform 1s ease'; 
         carouselInner.style.transform = `translateX(-${currentIndex * imageWidth * visibleImages}px)`;
 
-        // Si le carrousel atteint la dernière image, ramener la première image à côté de la dernière image avec une transition instantanée
+        
         if (currentIndex >= Math.ceil(totalImages / visibleImages)) {
             setTimeout(() => {
-                currentIndex = 0; // Réinitialiser l'indice
-                carouselInner.style.transition = 'none'; // Désactiver la transition pour un défilement instantané
+                currentIndex = 0; 
+                carouselInner.style.transition = 'none'; 
                 carouselInner.style.transform = `translateX(0)`;
-            }, 1000); // Délai pour permettre à la transition de se terminer
+            }, 1000); 
         }
     }, intervalTime);
 });
