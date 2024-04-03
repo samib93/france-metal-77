36da3be1451ecf250c63e385da1cf0ce3f22ede3
Samib93 <sami.barkaoui93290@gmail.com>
2024-04-03 14:19:20 +0200

    correction carrousel et ajout chapitre vision

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
     const carouselInner = document.querySelector('.carousel-inner');
-    const images = carouselInner.children;
+    const images = carouselInner.querySelectorAll('img');
     const totalImages = images.length;
     const imageWidth = images[0].offsetWidth; 
     const visibleImages = 3; 
     let currentIndex = 0;
 
     
+    for (let i = 0; i < visibleImages; i++) {
+        const clone = images[i].cloneNode(true);
+        carouselInner.appendChild(clone);
+    }
+
+   
     const intervalTime = 5000; 
 
     setInterval(function() {
         currentIndex++;
 
-       
-        carouselInner.style.transition = 'transform 1s ease'; 
-        carouselInner.style.transform = `translateX(-${currentIndex * imageWidth * visibleImages}px)`;
-
         
-        if (currentIndex >= Math.ceil(totalImages / visibleImages)) {
+        carouselInner.style.transition = 'transform 1s ease'; 
+        carouselInner.style.transform = `translateX(-${currentIndex * imageWidth}px)`;
+
+       
+        if (currentIndex >= totalImages) {
             setTimeout(() => {
                 currentIndex = 0; 
                 carouselInner.style.transition = 'none'; 
