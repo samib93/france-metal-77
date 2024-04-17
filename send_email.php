5841a1bd4d2d168975411bd1742bf634f78b7eea
Samib93 <sami.barkaoui93290@gmail.com>
2024-04-17 17:38:54 +0200

    ajout formulaire contact

--- send_email.php
+++ send_email.php
@@ -1,40 +1,23 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Récupérer les données du formulaire
-    $nom = $_POST['nom'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
+    // Récupération des données du formulaire
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
 
     // Adresse email de destination
-    $destinataire = "s.barkaoui@outlook.fr";
+    $to = "s_barkaoui@outlook.fr";
 
-    // Sujet de l'email
-    $sujet = "Nouveau message de $nom";
+    // Entête du mail
+    $headers = "From: $email \r\n";
+    $headers .= "Reply-To: $email \r\n";
+    $headers .= "Content-type: text/html; charset=UTF-8 \r\n";
 
-    // Corps de l'email
-    $corps_message = "Nom: $nom\n";
-    $corps_message .= "Email: $email\n\n";
-    $corps_message .= "Message:\n$message\n";
-
-    // Définir les paramètres SMTP
-    ini_set("SMTP", "smtp.office365.com");
-    ini_set("smtp_port", "587");
-
-    // En-têtes de l'email
-    $headers = "From: $email\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
-
-    // Envoyer l'email
-    $resultat = mail($destinataire, $sujet, $corps_message, $headers);
-
-    // Vérifier si l'email a été envoyé avec succès
-    if ($resultat) {
-        header('Location: confirmation.html');
-        exit;
+    // Envoi de l'email
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Votre message a bien été envoyé.";
     } else {
-        // Erreur lors de l'envoi de l'email
-        echo "Erreur lors de l'envoi de l'email";
+        echo "Erreur lors de l'envoi du message. Veuillez réessayer.";
     }
 }
 ?>
